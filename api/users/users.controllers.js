81a90e86da597786f6272ff1c12e335b4325458f
Ghaalia <Ghaliaals9@gmail.com>
2023-11-05 18:24:32 +0300

    ..

--- api/users/users.controllers.js
+++ api/users/users.controllers.js
@@ -1,9 +1,32 @@
 const User = require("../../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-exports.signup = async (req, res) => {
+const hashedPassword = async (password) => {
+  const newHashedPassword = await bcrypt.hash(password, 10);
+  return newHashedPassword;
+};
+
+const generateToken = (user) => {
+  const payload = {
+    _id: user._id,
+    username: user.username,
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_PRIVATE, {
+    expiresIn: process.env.JWT_TOKEN_EXP,
+  });
+
+  return token;
+};
+
+exports.signup = async (req, res, next) => {
   try {
+    req.body.password = await hashedPassword(req.body.password);
     const newUser = await User.create(req.body);
-    res.status(201).json(newUser);
+    const token = generateToken(newUser);
+
+    res.status(201).json({ token });
   } catch (err) {
     next(err);
   }
